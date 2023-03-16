4475a30cc913bec6abbb725f315c3aebee83e9fd
mdminhaziftekhar <mdminhaziftekhar@gmail.com>
2023-03-17 03:43:15 +0600

    Api key updated

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,2 +1,2 @@
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "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";
